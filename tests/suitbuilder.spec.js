ba9d816f72bc9d12360b7f81c4e16174da820f3f
Shakib-ewu <hossainsakib58@gmail.com>
2026-07-30 19:07:30 +0600

    test case updated

--- tests/suitbuilder.spec.js
+++ tests/suitbuilder.spec.js
@@ -193,7 +193,7 @@ test.skip("Save The Look persists current configuration", async ({ page }) => {
    await page.getByRole('button', { name: /save the look/i }).click();
 });
 
-test.only("End-to-end: configure suit, complete fit quiz, and buy and checkout", async ({ page }) => {
+test("End-to-end: configure suit, complete fit quiz, and buy and checkout", async ({ page }) => {
   test.setTimeout(120000);
 
   const suitSwatches = await getSuitSwatches(page);
